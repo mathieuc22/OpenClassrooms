889cb511a9c275f3af8c84a51fad247ffa628afa
Mathieu <mathieu.collet@gmail.com>
2021-08-06 16:43:38 +0200

    mise en place du parcours utilisateur

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
-});
+mongoose.connect('mongodb+srv://mathieu:mathieuc@cluster0.bag8u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 
 app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
