395206c6187109eaa643f102a84ab05964d658c7
Mathieu <mathieu.collet@gmail.com>
2021-08-31 12:14:16 +0200

    Correction du middleware d'authentification et simplification de la vérification

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -5,11 +5,7 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     const decodedToken = jwt.verify(token, process.env.SECRET_KEY);
     const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw "Identifiant utilsateur non valide";
-    } else {
-      next();
-    }
+    next();
   } catch {
     res.status(401).json({
       message: "Vous n'êtes pas connecté"
