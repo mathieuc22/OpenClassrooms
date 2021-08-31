e2a04a0f6ee1e621f399163c87908688b3ba213a
Mathieu <mathieu.collet@gmail.com>
2021-08-31 12:18:10 +0200

    Log de la synchronisation de la base de données

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,9 @@ sequelize.authenticate()
   .then(() => console.log('Connection has been established successfully.'))
   .catch((error) => console.error('Unable to connect to the database:', error))
 
-sequelize.sync();
+sequelize.sync({ force: true }).then(() => {
+  console.log("Databse droppée et resynchronisée");
+});
 
 // routes
 const postRoutes = require('./routes/post');
