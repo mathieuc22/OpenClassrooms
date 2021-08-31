317853b122ea7031246fd83792407f27c1c1bec0
Mathieu <mathieu.collet@gmail.com>
2021-08-31 12:16:54 +0200

    Ajout d'une relation avec la table User

--- backend/models/comment.js
+++ backend/models/comment.js
@@ -1,16 +1,23 @@
 const Sequelize = require('sequelize');
 const sequelize = require('../middleware/database');
+const User = require('../models/user')
 
 const Comment = sequelize.define('comment', {
-  author: {
-    type: Sequelize.STRING
-  },
   parent_comment: {
     type: Sequelize.STRING
   },
-  like: {
+  text: {
     type: Sequelize.STRING
   },
 })
+
+// relation le post doit avoir un utilisateur du model Post
+Comment.belongsTo(User, {
+  foreignKey: {
+    name: 'author',
+    type: Sequelize.UUID,
+    allowNull: false
+  }
+});
 
 module.exports = Comment;
