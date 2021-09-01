0499d8438e14a37be2816dd38114b9f4bf5c026e
Mathieu <mathieu.collet@gmail.com>
2021-09-01 21:02:04 +0200

    Ajout d'includes pour restituer le username côté frontend

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -16,7 +16,9 @@ exports.create = (req, res) => {
 
 // Retrieve all Posts from the database.
 exports.findAll = (req, res) => {
-  Post.findAll()
+  Post.findAll({ include: { model: User,
+    attributes: ['username'],
+    as: 'author' } })
     .then(posts => res.status(200).json({ posts: posts }))
     .catch(error => res.status(400).json({ error }));
 };
@@ -24,12 +26,25 @@ exports.findAll = (req, res) => {
 // Find a single Posts with an id
 exports.findOne = (req, res) => {
   const id = req.params.id;
-  Post.findByPk(id, { include: [ Comment, { model: User,
-    attributes: ['id'],
-    through: {
-      attributes: []
-    } ,
-    as: 'likes' } ] })
+  Post.findByPk(id, { include: [ 
+    {
+      model: Comment,
+      include: {
+        model: User,
+        attributes: ['username'],
+        as: 'author'
+      }
+    },
+    { model: User,
+      attributes: ['id'],
+      through: {
+        attributes: []
+      },
+      as: 'likes' },
+    { model: User,
+    attributes: ['username'],
+    as: 'author' }
+   ] })
     .then(post => {
       if (post) {
         res.status(200).json({ post: post })
