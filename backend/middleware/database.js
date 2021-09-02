f7965229c9a89574751f9f065fbcaea586b05ff5
Mathieu <mathieu.collet@gmail.com>
2021-09-02 16:48:54 +0200

    Passage a une base de donnée postgresql et fin du drop des tables

--- backend/middleware/database.js
+++ backend/middleware/database.js
@@ -1,6 +1,13 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
+const { DB_HOST, DB_DATABASE, DB_USERNAME, DB_PASSWORD } = process.env;
 
-module.exports = new Sequelize('sqlite::memory:', {
-  // Displays the first parameter of the log function call
-  logging: console.log,
+// module.exports = new Sequelize('sqlite::memory:', {
+//   // Displays the first parameter of the log function call
+//   logging: console.log,
+// });
+
+module.exports = new Sequelize(DB_DATABASE, DB_USERNAME, DB_PASSWORD, {
+  dialect: "postgres",
+  host: DB_HOST
 });
