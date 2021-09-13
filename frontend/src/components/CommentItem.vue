a96726436dc88f519491f4e2a99457390ac1c208
Mathieu <mathieu.collet@gmail.com>
2021-09-13 14:48:12 +0200

    Maquette de la page de détail d'un post et création d'un composant pour les commentaires

---
+++ frontend/src/components/CommentItem.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="postDetail__comments">
+    <h3>Commentaires</h3>
+    <form
+      @submit.prevent="submitComment"
+    >
+      <input
+        type="text"
+        id="text"
+        placeholder="Text"
+        v-model="comment.text"
+      />
+      <!-- Post button -->
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+    <ul>
+    <li
+      v-for="comment in allComments"
+      v-bind:key="comment.id"
+    >
+      <small><i class="fas fa-user"></i> {{ comment.author.username }} - {{ formatDate(comment.createdAt) }}</small>
+      <p>{{ comment.text }}</p>
+    </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Functions from '../functions/functions';
+export default {
+  name: 'CommentItem',
+  props: ['comments'],
+  mixins:[Functions],
+  data: function () {
+    return {
+      loaded: false,
+      userLike: "",
+      nbLikes: "",
+      isActive: false,
+      comment: {
+        text: ""
+      },
+      allComments: this.comments
+    };
+  },
+  computed: {
+  },
+  mounted() {
+  },
+  methods: {
+    // création d'un nouveau post avec l'API
+    submitComment() {
+      // récupère du store le token d'authentification pour la requête
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.$store.getters.user.token;
+      // envoie les données du formulaire à l'API
+      axios.post("/" + this.$route.params.id, this.comment)
+      .then(response => {
+        console.log(response);
+        // ajoute le nouvel élément à la liste des commentaires
+        let newComment = response.data.comment;
+        console.log(newComment);
+        newComment.author = {username: this.$store.getters.user.name};
+        console.log(newComment);
+        this.allComments.unshift(newComment);
+        this.comment = ''
+      })
+      // si une erreur est retournée, elle est restituée sur la page et on force la déconnexion
+      .catch(error => {
+        if (error.response) {
+          this.errorMessage = error.response.data.message;
+        } else {
+          this.errorMessage = error;
+        }
+        this.$store.dispatch('logOut');
+      });
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
--- frontend/src/views/Post.vue
+++ frontend/src/views/Post.vue
@@ -1,29 +1,38 @@
 <template>
-  <div v-if="errorMessage">
-    {{errorMessage}}
-  </div>
-  <div v-else-if="!loaded">
-    <p>Post is loading...</p>
-  </div>
-  <div v-else>
-    <h2>{{ post.title }}</h2>
-    <h3>Author: {{ post.author.username }}</h3>
-    <p>Likes: {{ nbLikes }}</p>
-    <p @click="likePost(post.id)">
-      {{ userLike }}
-    </p>
-    <small>{{ formatDate(post.createdAt) }}</small>
-    <p>{{ post.text }}</p>
-    <h3>Comments:</h3>
-    <ul v-for="comment in post.comments" :key="comment.id">
-      <li>{{ comment.text }} - {{ comment.author.username }}</li>
-    </ul>
+  <div class="postView">
+    <div v-if="errorMessage">
+      {{errorMessage}}
+    </div>
+    <div v-else-if="!loaded">
+      <p>Post is loading...</p>
+    </div>
+    <div v-else class="postDetail">
+      <div class="postDetail__post">
+        <h2>{{ post.title }}</h2>
+        <div class="postDetail__author">
+          <small>Publié par {{post.author.username}} le {{ formatDate(post.createdAt) }}</small>
+        </div>
+        <div class="postDetail__likes">
+          <span>{{ nbLikes }}</span>
+          <i
+            class="fa-heart like"
+            v-bind:class="{ 'like--active': isActive, 'far': !isActive, 'fas': isActive }"
+            @click="likePost(post.id)">
+          </i>
+        </div>
+        <p>{{ post.text }}</p>
+      </div>
+      <CommentItem
+        v-bind:comments="post.comments"
+      ></CommentItem>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 import Functions from '../functions/functions';
+import CommentItem from '@/components/CommentItem.vue'
 export default {
   name: "Post",
   mixins:[Functions],
@@ -34,21 +43,19 @@ export default {
       userLike: ' ',
       nbLikes: '',
       errorMessage: '',
+      isActive: false,
     };
+  },
+  components: {
+    CommentItem
   },
   computed: {
     user() {
       return this.$store.getters.user;
     },
-    // post() {
-    //   return this.$store.getters.post || '';
-    // },
     isAuthor() {
       return this.post.authorId === this.$store.getters.user.id;
     },
-  },
-  created() {
-    // this.$store.dispatch('loadPost', this.$route.params.id);
   },
   mounted() {
     // récupéation du token depuis le store vuex
@@ -58,9 +65,9 @@ export default {
       this.post = response.data.post;
       this.loaded = true;
       if (this.post.likes.some(x => x.id === this.user.id)) {
-        this.userLike = 'Unlike';
+        this.isActive = true;
       } else {
-        this.userLike = 'Like';
+        this.isActive = false;
       }
       this.nbLikes = this.post.likes.length;
     })
@@ -76,3 +83,46 @@ export default {
   },
 };
 </script>
+
+<style lang="scss">
+
+@import '../assets/variables.scss';
+
+.postView {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+
+.postDetail {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  width: 100%;
+  & > * {
+    width: 100%;
+    max-width: 800px;
+    display: flex;
+    flex-direction: column;
+    background: white;
+    border-radius: 5px;
+    font-size: 0.8em;
+    border: 1px solid $primary-color;
+    box-shadow: 0px 4px 4px 0px rgba(0,0,0,0.25);
+    overflow: hidden;
+    padding: 20px;
+  }
+}
+
+.like {
+  color: $primary-color;
+  cursor: pointer;
+}
+
+.like--active {
+  color: hotpink;
+}
+
+</style>
